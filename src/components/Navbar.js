554c1bd6eb97eac69d45a246a6a46ae13e5475eb
Giovanni Garcia de Souza Pastore <ggspastore@gmail.com>
2021-06-21 00:44:10 -0300

    Feat(Router): added authentication in all the site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom";
 import {
   Container,
   Navbar,
@@ -7,49 +7,90 @@ import {
   ButtonGroup,
   Button,
   DropdownButton,
-  Dropdown
+  Dropdown,
+  Form,
 } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import "./Navbar.scss";
 import logo from "../assets/logo.svg";
 
-const Navigation = ({ handleCreateAcc }) => (
+const Navigation = ({ handleCreateAcc, logged, auth }) => {
+
+  const history = useHistory;
   
+  const handleClick = () => {
+    auth.logout(() => history.push('/'));
+  };
+
+  return (
   <Navbar expand="lg">
-  <Container>
-    <Navbar.Brand>        
-      <Link to='/' exact>
-        <img
-        src={logo}
-        height="30"
-        className="d-inline-block align-top"
-        alt="Magbank logo"
-        />
-      </Link>
-    </Navbar.Brand>
+    <Container>
+      <Navbar.Brand>
+        <Link to="/" exact>
+          <img
+            src={logo}
+            height="30"
+            className="d-inline-block align-top"
+            alt="Magbank logo"
+          />
+        </Link>
+      </Navbar.Brand>
 
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      {logged && (
+        <>
+          <Form>
+            <div className="navbar__search-group d-none d-lg-flex">
+              <Form.Control type="text" placeholder="O que você procura?" />
+              <Button variant="link">
+                <FontAwesomeIcon icon={faSearch} color="#FFF" />
+              </Button>
+            </div>
+          </Form>
+          <Button variant="outline-light" onClick={handleClick}>Sair</Button>
+        </>
+      )}
 
-    <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className='nav__margin'>
-            <Nav.Link href="#cartao" className='text-light'>Cartão</Nav.Link>
-            <Nav.Link href="#quem-somos" className='text-light'>Quem Somos</Nav.Link>
-            <Nav.Link href="#faq" className='text-light'>FAQ</Nav.Link>
-          </Nav>
+      {!logged && (
+        <>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="nav__margin">
+              <Nav.Link href="#cartao" className="text-light">
+                Cartão
+              </Nav.Link>
+              <Nav.Link href="#quem-somos" className="text-light">
+                Quem Somos
+              </Nav.Link>
+              <Nav.Link href="#faq" className="text-light">
+                FAQ
+              </Nav.Link>
+            </Nav>
 
-          <ButtonGroup aria-label="Basic example" className='nav__margin'>
-            <DropdownButton id="dropdown-basic-button" variant="outline-light" title="Acessar minha conta">
-              <Dropdown.Item><Link to='/login'>Pessoa Física</Link></Dropdown.Item>
-              <Dropdown.Item><Link to='/login'>Pessoa Jurídica</Link></Dropdown.Item>
-            </DropdownButton>
-            <Button variant="outline-light" onClick={handleCreateAcc}>
-              abra sua conta
-            </Button>
-          </ButtonGroup>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+            <ButtonGroup aria-label="Basic example" className="nav__margin">
+              <DropdownButton
+                id="dropdown-basic-button"
+                variant="outline-light"
+                title="Acessar minha conta"
+              >
+                <Dropdown.Item>
+                  <Link to="/login">Pessoa Física</Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <Link to="/login">Pessoa Jurídica</Link>
+                </Dropdown.Item>
+              </DropdownButton>
+                <Button variant="outline-light" onClick={handleCreateAcc}>
+                  abra sua conta
+                </Button>
+            </ButtonGroup>
+          </Navbar.Collapse>
+        </>
+      )}
 
-
-);
+    </Container>
+  </Navbar>
+)};
 
 export default Navigation;
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,43 +1,67 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import {
-  Row,
-  Col,
-  Form,
-  Button,
-  Image
-} from "react-bootstrap";
-import logo from '../assets/logo.svg'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Row, Col, Form, Button, Image } from "react-bootstrap";
+import logo from "../assets/logo.svg";
 
-import './Login.scss';
+import "./Login.scss";
 
-const Login = () => (
-    <section className='login'>
-        <div className='login__container'>
-            <Row>
-                <Col className='text-center text-light'>
-                    <Image src={logo} className='mb-5' />
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label className='lead'><strong>Número da conta</strong></Form.Label>
-                            <Form.Control type="number" />
-                        </Form.Group>
+const Login = ({ auth }) => {
+  const [name, setName] = useState();
+  const [account, setAccount] = useState();
+  const history = useHistory();
+  const handleSubmit = () => {
+    auth.login(name, account, history.push("/dashboard"));
+  };
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label className='lead'><strong>Password</strong></Form.Label>
-                            <Form.Control type="password" />
-                        </Form.Group>
-                        <Link to='/dashboard' exact>
-                        <Button variant="success" type="submit" className='mt-3'>
-                            Criar conta
-                        </Button>
-                        </Link>
-                    </Form>
-                </Col>
-            </Row>
-        </div>
+  return (
+    <section className="login">
+      <div className="login__container">
+        <Row>
+          <Col className="text-center text-light">
+            <Image src={logo} className="mb-5" />
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label className="lead">
+                  <strong>Nome e Sobrenome</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.currentTarget.value)}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicAccountNumber">
+                <Form.Label className="lead">
+                  <strong>Número da Conta</strong>
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  value={account}
+                  onChange={(e) => setAccount(e.currentTarget.value)}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label className="lead">
+                  <strong>Password</strong>
+                </Form.Label>
+                <Form.Control type="password" />
+              </Form.Group>
+              <Button
+                variant="success"
+                type="submit"
+                className="mt-3"
+                onClick={handleSubmit}
+              >
+                Entrar
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </section>
-);
-
+  );
+};
 
 export default Login;
